202f019d8013581d61c15c4e8db45e0ee56fb41a
kevinMejia <59616614+KevinMejia6400@users.noreply.github.com>
2020-03-09 18:13:03 -0600

    css

--- tarea7/src/Componentes/RenderisarImagenes.js
+++ tarea7/src/Componentes/RenderisarImagenes.js
@@ -3,9 +3,11 @@ import React from 'react';
 function RerendisarImagenes(props){
     const imagen = props.valor;
     const retorno = (
-        <img src={imagen.link} alt="..." class="img-thumbnail"></img>
+        <img src={imagen.link} alt="..." class="img-thumbnail" style={{width: 300, height: 300}}></img>
     );
     return retorno;
 }
+
+
 
 export default RerendisarImagenes;
